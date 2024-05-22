4f3bf9672148cee14d97f4f17b272a2efdd9f55e
ogola <ogolajeremy601@gmail.com>
2024-05-22 20:33:36 +0300

    adds sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0 :
+        print([])
+        return
+    fib_seq = [0, 1]
+
+    while len(fib_seq) < length:
+        next_fib = fib_seq[-1] + fib_seq[-2]
+        fib_seq.append(next_fib)
+
+    if length == 1:
+        fib_seq = [0]
+
+    print(fib_seq[:length])
+
+print_fibonacci(10)
